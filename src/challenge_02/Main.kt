e7d862cbbf4957475f8e3f48639877e105d12f82
pablosilva <pablo.silva.rodrigo@gmail.com>
2020-05-21 13:38:49 -0400

    [CesarChallenge02][Resolved][Kotlin]
    Developed Check words with jumbled letters

---
+++ src/challenge_02/Main.kt
@@ -0,0 +1,44 @@
+package challenge_02
+
+fun main() {
+
+    println(isPartialPermutation("you", "yuo"))
+    println(isPartialPermutation("probably", "porbalby"))
+    println(isPartialPermutation("despite", "desptie"))
+    println(isPartialPermutation("moon", "nmoo"))
+    println(isPartialPermutation("misspellings", "mpeissngslli"))
+
+}
+
+fun isPartialPermutation(word: String, jumbledWord: String): Boolean {
+
+    // To be permutation it should have the same length and The first letter hasn’t changed place
+    if (word.length == jumbledWord.length && word[0] == jumbledWord[0]) {
+        val lengthOfTheWords = word.length;
+
+        var countDifferentLetter = 0
+
+
+        jumbledWord.forEachIndexed { index, letter ->
+            if (word.indexOf(letter) == -1) {
+                return false;
+            }
+            if (index != word.indexOf(letter)) {
+                countDifferentLetter++
+            }
+        }
+
+        // If word has more than 3 letters, up to 2/3 of the letters have changed place
+        if (lengthOfTheWords > 3) {
+            if (countDifferentLetter < (lengthOfTheWords * (2f / 3f))) {
+                return true
+            }
+        } else {
+            return countDifferentLetter > 0
+        }
+    }
+
+    return false
+
+
+}
